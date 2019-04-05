367479bef00ca004c6191716496ec3fb53e76eea
Albert Lie <alberttri23@gmail.com>
2019-04-05 17:44:52 +0700

    add postgres connection string in log

--- src/main/java/com/albertlie/RunnablePostgres.java
+++ src/main/java/com/albertlie/RunnablePostgres.java
@@ -3,13 +3,14 @@ package com.albertlie;
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
 
 public class RunnablePostgres {
-	public static void main (String[] args) {
-		EmbeddedPostgres postgres = new EmbeddedPostgres();
+    public static void main (String[] args) {
+        EmbeddedPostgres postgres = new EmbeddedPostgres();
 
-		try {
-			String url = postgres.start("localhost", 5432, "mock-postgres");
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
+        try {
+            String url = postgres.start("localhost", 5432, "mock-postgres");
+            System.out.println("You could connect to your postgres with this connection string: " + url);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
